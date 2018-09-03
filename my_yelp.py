1ab1734c6c40fb6e88125f6d8c522c8dfecdd104
Qamber Mehdi <qmehdi110@gmail.com>
2018-09-02 21:50:02 -0500

    added requirements.txt, Procfile, and changes to run on heroku

--- my_yelp.py
+++ my_yelp.py
@@ -24,9 +24,9 @@ def search_businesses(search_term, search_location):
     businesses_dict = json.loads(businesses_object.text)
 
     businesses_list = businesses_dict['businesses']
-    # 
+    
+    print(businesses_dict)
     list_of_businesses = []
     for each in businesses_list:
         list_of_businesses.append(each)
     return list_of_businesses
-
